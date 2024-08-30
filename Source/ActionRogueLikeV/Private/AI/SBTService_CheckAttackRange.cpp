f304c69aa31a13febc4d02526964d5c4c9df67df
mobinabiuk <mobinboyok@gmail.com>
2024-08-30 18:25:01 +0330

    AI enemy attacking player

--- Source/ActionRogueLikeV/Private/AI/SBTService_CheckAttackRange.cpp
+++ Source/ActionRogueLikeV/Private/AI/SBTService_CheckAttackRange.cpp
@@ -23,7 +23,7 @@ void USBTService_CheckAttackRange::TickNode(UBehaviorTreeComponent& OwnerComp, u
 				if (ensure(AIPawn))
 				{
 					float DistanceTo = FVector:: Distance(TargetActor->GetActorLocation(), AIPawn->GetActorLocation());
-					bool bWithinRange = DistanceTo < 2000.f;
+					bool bWithinRange = DistanceTo < 1000.f;
 					bool bHasLOS = false;
 					if (bWithinRange)
 					{
